c24adf46dac0eaea608c0de7769a7db090c7d7e8
Sayeed Tahmid <sayeedtahmid07@gmail.com>
2026-04-24 04:44:14 +0600

    Mobile app working with few layout problems

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.syedtahmidmanzoor.ossimulator',
+  appName: 'OS Algorithm Simulator',
+  webDir: 'dist'
+};
+
+export default config;
